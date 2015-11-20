5a2564f57a599590feffb71aad5a1ab60ff599ef
Emanuel Rancov <emanuel.rancov@evozon.com>
2015-11-20 20:50:44 +0200

    configured gulp to start mongo and server

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
-var gulp  = require('gulp');
-var exec  = require('child_process').exec;
-var spawn = require("gulp-spawn").spawn;
+var gulp             = require('gulp');
+var exec             = require('child_process').exec;
+var spawn          = require("gulp-spawn").spawn;
+var gutil              = require('gulp-util');
+var colors           = require('colors');
+var mongoPath = "\"C:\\Program Files\\MongoDB\\Server\\3.0\\bin\\mongod.exe\"";
 
   gulp.task('mongo', function (cb) {
-    exec('start mongod --dbpath ', function (err, stdout, stderr) {
-   console.log(stdout);
-   console.log(stderr);
-   cb(err);
- });
+  gutil.log("@@@Running on port :27017@@@" .green);
+    exec(mongoPath, function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
   });
+
+  gulp.task('server', function (cb) {
+  gutil.log("@@@Running on port :1337@@@".magenta);
+    exec('node server.js', function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  });
+
+  gulp.task('app', ['mongo', 'server']);
