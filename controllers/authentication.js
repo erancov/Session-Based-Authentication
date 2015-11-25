9ba946484384c61eb0c146bccded7b7f6c76dc7e
Emanuel Rancov <emanuel.rancov@evozon.com>
2015-11-25 18:08:40 +0200

    fixed conflict

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -10,12 +10,19 @@ module.exports.logout = logout;
 
 
  function login(req, res, next) {
-   res.render('login', {message : req.session.messages});
+    console.log(req.session.messages);
+   res.render('login');
 }
 
 function loggedIn(req, res, next) {
-    res.render('dashboard');
+  console.log(req.user);
     req.session.messages = [];
+  if(req.isAuthenticated()) {
+    res.render('dashboard');
+  }else {
+    res.redirect('/login');
+  }
+
 }
 
 function logout(req, res, next) {
